5db35086782bfe68d676a47aaee8d0ca4ce6ea34
Rene Haavre <haavre@gmail.com>
2018-05-29 20:06:36 +0300

    Initial Commit

---
+++ CustomControlStackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomControlStackView
+//
+//  Created by Rene Haavre on 29/05/2018.
+//  Copyright © 2018 Rene Haavre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
